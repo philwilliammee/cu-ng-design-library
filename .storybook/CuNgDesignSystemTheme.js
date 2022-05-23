e558aebef9c391e2ebd17abc1e06bf31b69010ee
philwilliammee <phil.williammee@gmail.com>
2022-05-22 21:32:03 -0400

    personailze theme

---
+++ .storybook/CuNgDesignSystemTheme.js
@@ -0,0 +1,15 @@
+import { create } from "@storybook/theming";
+
+// const svg =
+//   "https://drive.google.com/file/d/157EPtVqITjDDTtItUJjHR7ngggPkXh39/view?usp=sharing";
+// const png =
+//   "https://drive.google.com/file/d/1a8qr7UKAc9eXuX5esVRwurnmCSgPFbR6/view?usp=sharing";
+export default create({
+  base: "dark",
+  brandTitle: "CU NG Design Library",
+  brandUrl:
+    "https://philwilliammee.github.io/cu-ng-design-library/?path=/story/cu-ng-design-library-installation--page",
+  // @todo make a custom icon
+  brandImage:
+    "https://upload.wikimedia.org/wikipedia/commons/c/ca/AngularJS_logo.svg",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from "@storybook/addons";
+import { themes } from "@storybook/theming";
+import CuNgDesignSystemTheme from "./CuNgDesignSystemTheme";
+
+addons.setConfig({
+  theme: CuNgDesignSystemTheme,
+});
