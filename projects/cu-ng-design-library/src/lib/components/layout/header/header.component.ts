bdd085e041a394798487c638deb46cbf7f5f9248
philwilliammee <phil.williammee@gmail.com>
2022-05-27 10:30:55 -0400

    add header options

--- projects/cu-ng-design-library/src/lib/components/layout/header/header.component.ts
+++ projects/cu-ng-design-library/src/lib/components/layout/header/header.component.ts
@@ -1,5 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+// Cornell Branding Banner
+// .cu-red - Overrides the default light gray banner to be red with white logo. Affects both 45px and Large Seal options.
+// .cu-black - Overrides the default light gray banner to be black with white logo. Affects both 45px and Large Seal options.
+// .cu-gray - Overrides the default light gray banner to be dark gray with white logo. Affects both 45px and Large Seal options.
+// .cu-45 - Renders as a ~45px banner with small Cornell logo (this is also the default if no body class is defined). Headings appear below the banner. Note: This class is mutually exclusive with .cu-seal.
+// .cu-seal - Renders as a taller banner with a large standalone Cornell seal. Headings appear to the right of the seal. Note: This class is mutually exclusive with .cu-45.
+// .cu-seal-right - Added to .cu-seal to swap the left and right positions of the seal and headings.
+// .cu-45-mobile - Added to .cu-seal to switch to 45px style at mobile sizes.
+// .cu-45-mobile-red - Added to .cu-seal-mobile also activate the red color option on the 45px banner at mobile sizes.
+// .no-main-nav - When .cu-seal is in use and no main navigation bar is planned, this class can be included to apply some subtle adjustments to the Cornell header.
+
+type theme = 'cu-red' | 'cu-black' | 'cu-gray';
+type logo = 'cu-seal' | 'cu-45';
+
 @Component({
   selector: 'lib-header',
   templateUrl: './header.component.html',
@@ -10,11 +24,15 @@ export class HeaderComponent implements OnInit {
     'https://rawcdn.githack.com/CU-CommunityApps/cwd_base/734e40e6a6e21bf5cc8afff65096b452d864480e/images/cornell/bold_cornell_logo_simple_b31b1b.svg';
   @Input() title!: string;
   @Input() subtitle!: string;
+  @Input() theme!: theme;
+  @Input() logo!: logo;
   constructor() {
     this.title = this.title || 'CU-NG-Design-Library';
     this.subtitle =
       this.subtitle ||
       'A library of reusable components for the CU-Community-Apps';
+    this.theme = this.theme || 'cu-gray';
+    this.logo = this.logo || 'cu-seal';
   }
 
   ngOnInit(): void {}
--- stories/header.stories.ts
+++ stories/header.stories.ts
@@ -12,6 +12,30 @@ export default {
       imports: [HeaderModule],
     }),
   ],
+  argTypes: {
+    title: {
+      control: {
+        type: 'text',
+      },
+    },
+    subtitle: {
+      control: {
+        type: 'text',
+      },
+    },
+    theme: {
+      control: {
+        type: 'select',
+        options: ['cu-red', 'cu-black', 'cu-gray'],
+      },
+    },
+    logo: {
+      control: {
+        type: 'select',
+        options: ['cu-seal', 'cu-45'],
+      },
+    },
+  },
   // parameters: {
   //   docs: {
   //     page: HeaderDocumentation,
@@ -25,7 +49,18 @@ const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
 
 export const Default = Template.bind({});
 
+export const Cu45 = Template.bind({});
+
 Default.args = {
   title: 'CU-NG-Design-Library',
   subtitle: 'A library of reusable components for the CU-Community-Apps',
+  theme: 'cu-gray',
+  logo: 'cu-seal',
+} as Partial<HeaderComponent>;
+
+Cu45.args = {
+  title: 'CU-NG-Design-Library',
+  subtitle: 'A library of reusable components for the CU-Community-Apps',
+  theme: 'cu-red',
+  logo: 'cu-45',
 } as Partial<HeaderComponent>;
