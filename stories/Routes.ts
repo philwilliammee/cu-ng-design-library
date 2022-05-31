1ae23d09d31d7e1629dab397300f7a3f173f4f32
philwilliammee <phil.williammee@gmail.com>
2022-05-31 08:03:46 -0400

    sub-nav

--- stories/Routes.ts
+++ stories/Routes.ts
@@ -6,20 +6,41 @@ export const routes: Routes = [
   {
     path: 'two-col-page',
     component: TwoColumnComponent,
-    data: { menu: 'main' },
+    data: { menu: 'main', title: 'Two Column Page' },
   },
   {
     path: 'one-col-page',
     component: OneColumnComponent,
-    data: { menu: 'main' },
+    data: {
+      menu: 'main',
+      title: 'One Column Page',
+    },
+    children: [
+      {
+        path: 'nested',
+        component: TwoColumnComponent,
+        data: {
+          menu: 'one-col-page',
+          title: 'Nested Page',
+        },
+      },
+      {
+        path: 'nested2',
+        component: OneColumnComponent,
+        data: {
+          menu: 'one-col-page',
+          title: 'Another Nested Page',
+        },
+      },
+    ],
   },
 
   {
     path: 'utility',
     component: OneColumnComponent,
-    data: { menu: 'utility' },
+    data: { menu: 'utility', title: 'Utility Page' },
   },
-  { path: '**', redirectTo: 'two-col-page', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'two-col-page', pathMatch: 'full' },
 ];
 
 export const RoutesModule = RouterModule.forRoot(routes);
